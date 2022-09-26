c53f049a9c877e5270f9aa312df1f09cfe3c7bee
ElianderG <eliandergomes@gmail.com>
2022-09-26 16:01:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function entnum(num1){
+    var display = document.getElementById("display")
+    var varcont = display;
+    if (display.value=="0"){
+        display.value = num1;
+    } else {
+        display.value = varcont.value + num1
+    }
+}
+function cleanall(){
+    var display = document.getElementById("display").value = 0;
+}
+
+function bracall(){
+    var display = document.getElementById("display")
+    var varcont = display;
+    if (braopen = true){
+
+    }
+}
